e62eefe2e5496892ec56634075d2bd90eb7f8808
dmsy4 <51166160+dmsy4@users.noreply.github.com>
2019-05-29 17:20:58 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,25 +1,26 @@
 import telebot
 import parsing
+import config
 
-bot = telebot.TeleBot('config.token')
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
     bot.send_message(message.chat.id, 'Чтобы посмотреть доступные функции напиши /help')
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('/MMR_ladder', '/counter_picks')
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard2.row('/Europe', '/SE_Asia', '/Americas', '/China')
+functions_list = telebot.types.ReplyKeyboardMarkup(True, True)
+functions_list.row('/MMR_ladder', '/counter_picks')
+regions_list = telebot.types.ReplyKeyboardMarkup(True, True)
+regions_list.row('/Europe', '/SE_Asia', '/Americas', '/China')
 
 @bot.message_handler(commands=['help'])
 def abilities(message):
-    bot.send_message(message.chat.id, 'На цифровой клавиатуре отображены доступные функции', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'На цифровой клавиатуре отображены доступные функции', reply_markup=functions_list)
 
 @bot.message_handler(commands=['MMR_ladder'])
 def foo_ladder(message):
-     bot.send_message(message.chat.id, 'Выбери регион', reply_markup=keyboard2)
+     bot.send_message(message.chat.id, 'Выбери регион', reply_markup=regions_list)
 
 @bot.message_handler(commands=['Europe', 'SE_Asia', 'Americas', 'China'])
 def foo_ladder_region(message):
